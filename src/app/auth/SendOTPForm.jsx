f16e54a5a5ab2367154f5e38f7219ba538c36c64
salarGholami <salargholami021@gmail.com>
2025-01-15 02:38:04 +0330

    SendOTPForm component

--- src/app/auth/SendOTPForm.jsx
+++ src/app/auth/SendOTPForm.jsx
@@ -1,15 +1,18 @@
 import TextFiled from "@/common/TextFiled";
 
-function SendOTPForm({ phoneNumber, onChange }) {
+function SendOTPForm({ phoneNumber, onChange, onSubmit }) {
   return (
     <div>
-      <form>
+      <form className="space-y-5" onSubmit={onSubmit}>
         <TextFiled
           label="شماره موبایل"
           name="phoneNumber"
           value={phoneNumber}
-          onChange={(e) => onChange(e.target.value)}
+          onChange={onChange}
         />
+        <button type="submit" className="btn btn--primary w-full">
+          ارسال کد تایید
+        </button>
       </form>
     </div>
   );
--- src/app/auth/page.jsx
+++ src/app/auth/page.jsx
@@ -1,17 +1,34 @@
 "use client";
+
 import { useState } from "react";
 import SendOTPForm from "./SendOTPForm";
+import http from "@/services/httpService";
 
 function AuthPage() {
   const [phoneNumber, setPhoneNumber] = useState("");
+  
   const phoneNumberHandler = (e) => {
     setPhoneNumber(e.target.value);
+  };
+
+  const sendOTPHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await http.post("/user/get-otp", { phoneNumber });
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <div className="flex justify-center">
       <div className="w-full sm:max-w-sm">
-        <SendOTPForm phoneNumber={phoneNumber} onChange={phoneNumberHandler} />
+        <SendOTPForm
+          phoneNumber={phoneNumber}
+          onChange={phoneNumberHandler}
+          onSubmit={sendOTPHandler}
+        />
       </div>
     </div>
   );

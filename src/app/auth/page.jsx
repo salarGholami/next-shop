1fd4950941b6f99d7af755e3a4a1bfc0c0b45aaa
salarGholami <salargholami021@gmail.com>
2025-01-15 03:43:02 +0330

    adding React-Query

--- src/app/auth/page.jsx
+++ src/app/auth/page.jsx
@@ -4,9 +4,14 @@ import { useState } from "react";
 import toast from "react-hot-toast";
 import http from "@/services/httpService";
 import SendOTPFrom from "./SendOTPForm";
+import { useMutation } from "@tanstack/react-query";
+import { getOTP } from "@/services/authServices";
 
 function AuthPage() {
   const [phoneNumber, setPhoneNumber] = useState("");
+  const { data, error, isLoading, mutateAsync } = useMutation({
+    mutationFn: getOTP,
+  });
 
   const phoneNumberHandler = (e) => {
     setPhoneNumber(e.target.value);
@@ -15,7 +20,7 @@ function AuthPage() {
   const sendOtpHandler = async (e) => {
     e.preventDefault();
     try {
-      const { data } = await http.post("/user/get-otp", { phoneNumber });
+      const data = await mutateAsync({phoneNumber});
       toast.success(data.data);
     } catch (error) {
       toast.error(error?.response?.data?.message);
---
+++ src/services/authServices.js
@@ -0,0 +1,5 @@
+import http from "./httpService";
+
+export function getOTP(data) {
+  return http.post("/user/get-otp", data);
+}

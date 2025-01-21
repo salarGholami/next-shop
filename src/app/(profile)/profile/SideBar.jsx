8be80ea78d8802b3ac5655e281569c6e17514e50
salarGholami <salargholami021@gmail.com>
2025-01-21 16:55:36 +0330

    User info page

--- src/app/(profile)/profile/SideBar.jsx
+++ src/app/(profile)/profile/SideBar.jsx
@@ -8,7 +8,7 @@ function SideBar() {
           <Link href="/">صفحه اصلی</Link>
         </li>
         <li>
-          <Link href="/me">اطلاعات کاربری</Link>
+          <Link href="/profile/me">اطلاعات کاربری</Link>
         </li>
       </ul>
     </div>

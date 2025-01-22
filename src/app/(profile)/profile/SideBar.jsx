3e83824334e51a73cca506bd02a731f861f1634c
salarGholami <salargholami021@gmail.com>
2025-01-22 06:41:12 +0330

    logout user from app

--- src/app/(profile)/profile/SideBar.jsx
+++ src/app/(profile)/profile/SideBar.jsx
@@ -1,4 +1,14 @@
+"use client";
+
+import { logout } from "@/services/authServices";
 import Link from "next/link";
+const logoutHandler = async () => {
+  await logout();
+  // localStorage.removeItem("userInfo");
+  // localStorage.removeItem("cartItems");
+  // localStorage.removeItem("token");
+  document.location.href = "/";
+};
 
 function SideBar() {
   return (
@@ -8,7 +18,13 @@ function SideBar() {
           <Link href="/">صفحه اصلی</Link>
         </li>
         <li>
+          <Link href="/profile">داشبورد</Link>
+        </li>
+        <li>
           <Link href="/profile/me">اطلاعات کاربری</Link>
+        </li>
+        <li>
+          <button onClick={logoutHandler}>خروج از حساب کاربری</button>
         </li>
       </ul>
     </div>

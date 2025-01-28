49f202c272959616097167e6a72a0ad197e61f0e
salarGholami <salargholami021@gmail.com>
2025-01-29 03:05:20 +0330

    sort products

---
+++ src/app/(user)/products/ProductSort.jsx
@@ -0,0 +1,71 @@
+"use client";
+
+import RadioInput from "@/common/RadioInput";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useCallback, useEffect, useState } from "react";
+
+const sortOptions = [
+  {
+    id: 1,
+    value: "latest",
+    label: "جدیدترین",
+  },
+  {
+    id: 2,
+    value: "earliest",
+    label: "قدیمی ترین",
+  },
+  {
+    id: 3,
+    value: "popular",
+    label: "محبوب ترین",
+  },
+];
+
+function ProductSort() {
+  const [sort, setSort] = useState("");
+
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const createQueryString = useCallback(
+    (name, value) => {
+      const params = new URLSearchParams(searchParams);
+      params.set(name, value);
+      return params.toString();
+    },
+    [searchParams]
+  );
+
+  const sortHandler = (e) => {
+    const value = e.target.value;
+    setSort(value);
+    router.push(pathname + "?" + createQueryString("sort", value));
+  };
+
+  useEffect(() => {
+    setSort(searchParams.get("sort") || "");
+  }, [searchParams]);
+
+  return (
+    <div>
+      <p className="font-bold mb-4">مرتب سازی</p>
+      {sortOptions.map((item) => {
+        return (
+          <RadioInput
+            key={item.id}
+            id={item.id}
+            label={item.label}
+            name="productSort"
+            value={item.value}
+            checked={sort === item.value}
+            onChange={sortHandler}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default ProductSort;

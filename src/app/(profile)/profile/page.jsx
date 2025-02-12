bf5f20dc007064d49e53ec80c89353dfbfe709a2
salarGholami <salargholami021@gmail.com>
2025-02-12 07:26:51 +0330

    Create dashboard page

--- src/app/(profile)/profile/page.jsx
+++ src/app/(profile)/profile/page.jsx
@@ -3,10 +3,12 @@
 import Loading from "@/common/Loading";
 import { useGetUser } from "@/hooks/useAuth";
 import { toLocalDateString } from "@/utils/toLocalDate";
+import PaymentTable from "./payments/PaymentTable";
+import Link from "next/link";
 
 function Profile() {
   const { data, isLoading } = useGetUser();
-  const { user } = data || {};
+  const { user, payments } = data || {};
 
   if (isLoading) return <Loading />;
 
@@ -16,6 +18,17 @@ function Profile() {
       <p>
         <span>تاریخ پیوستن : {toLocalDateString(user.createdAt)}</span>
       </p>
+      <div className="border rounded-xl p-4 mt-8">
+        <div className="flex items-center justify-between">
+          <h2 className="font-bold">آخرین سفارشات کاربر</h2>
+          <Link className="text-primary-900" href="/profile/payments">مشاهده همه سفارشات</Link>
+        </div>
+        <PaymentTable
+          payments={payments
+            .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+            .slice(0, 3)}
+        />
+      </div>
     </div>
   );
 }
